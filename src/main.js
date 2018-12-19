210aeab073853ca9acc78d1b02b04866a7411343
DavidYangYong <yyzhpq@163.com>
2018-12-19 11:27:55 +0800

    sucess

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
-
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
@@ -18,8 +16,6 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 Vue.use(ElementUI, { locale })
-
-
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
