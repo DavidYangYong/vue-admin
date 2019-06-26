6bd7e83231d3cb301cefb567a62c7d0bb395160e
DavidYangYong <yyzhpq@163.com>
2019-06-26 17:08:14 +0800

    test

--- src/api/login.js
+++ src/api/login.js
@@ -1,39 +1,41 @@
 import request from '@/utils/request'
 import ssoconfig from '@/config/config.js'
 import qs from 'qs'
-import { removeToken } from '@/utils/auth' // 验权
+import {
+  removeToken
+} from '@/utils/auth' // 验权1
 
 export function login(username, password) {
-  let  queryData= {
-            "grant_type": 'password',
-            "username": username,
-            "password": password
-        };
-    return request({
-        url: ssoconfig.userAuthorizationUri,
-        method: 'post',
-        　headers: {　　　　
-            'Content-Type': 'application/x-www-form-urlencoded'　　　
-        },
-        auth: {
-            username: 'wlm',
-            password: '18cf69f1ce412a0416740b917a33d853'
-        },
-        data:qs.stringify(queryData)
-    })
+  let queryData = {
+    "grant_type": 'password',
+    "username": username,
+    "password": password
+  };
+  return request({
+    url: ssoconfig.userAuthorizationUri,
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    auth: {
+      username: 'wlm',
+      password: '18cf69f1ce412a0416740b917a33d853'
+    },
+    data: qs.stringify(queryData)
+  })
 }
 export function getInfo(token) {
-    return request({
-        url: ssoconfig.userInfoUri,
-        method: 'get',
-        params: {
-            access_token:token
-        }
-    })
+  return request({
+    url: ssoconfig.userInfoUri,
+    method: 'get',
+    params: {
+      access_token: token
+    }
+  })
 }
 export function logout() {
-   // return request({
-   //     url: ssoconfig.logoutUri,
-   //     method: 'post'
-   // })
+  // return request({
+  //     url: ssoconfig.logoutUri,
+  //     method: 'post'
+  // })
 }
